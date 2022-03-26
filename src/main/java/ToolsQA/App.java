ede26da6fed3c31617b8f140fbe625d87402ab58
kasi3 <kasi3@LAPTOP-0UAIUMBF>
2022-03-26 12:53:40 -0600

    3er Cambio

--- src/main/java/ToolsQA/App.java
+++ src/main/java/ToolsQA/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola Kasandra2, eres el boss 2 " );
+        System.out.println( "Hola Kasandra3, eres el boss 2 " );
     }
 }
