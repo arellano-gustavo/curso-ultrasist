3e784d7bad39ffcc91c8af89c0b4e614059dd2e0
claul <claul@LAPTOP-BBIO5N3H>
2022-03-26 12:46:05 -0600

    cambiando el mensaje

--- src/main/java/ToolsQA/App.java
+++ src/main/java/ToolsQA/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola Goose, eres el boss" );
+        System.out.println( "Hola Claudia2, eres Claudia" );
     }
 }
