06842502aaba3adfcb1be67f00664f42c0313d9e
garellano <garellano@localhost>
2022-04-09 11:44:24 -0500

    primer commit con código que no hace nada pero si compila

--- src/main/java/curso/App.java
+++ src/main/java/curso/App.java
@@ -1,4 +1,4 @@
-package ToolsQA;
+package curso;
 
 public class App  {
     public static void main( String[] args ) {
--- src/test/java/curso/AppTest.java
+++ src/test/java/curso/AppTest.java
@@ -1,5 +1,6 @@
-package ToolsQA;
+package curso;
 
+import curso.App;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
